2da5e1dad79f388728f7f2de24baf1b1b62780fd
Raphael Saldanha <rfsaldanha@gmail.com>
2024-05-17 14:26:46 +0200

    Remove old test

--- tests/testthat/test-deposit_stats.R
+++ tests/testthat/test-deposit_stats.R
@@ -7,13 +7,3 @@ test_that("deposit_stats works", {
   expect_equal(nrow(res), 6)
   expect_equal(length(res), 6)
 })
-
-test_that("deposit_stats works in parallel", {
-  testthat::skip_on_cran()
-
-  ids <- c(11190594, 11193971, 11126924)
-  res <- deposit_stats(deposit_ids = ids, progress = FALSE)
-
-  expect_equal(nrow(res), 6)
-  expect_equal(length(res), 6)
-})
